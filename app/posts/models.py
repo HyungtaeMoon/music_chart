4546e14fa26dbf9e13cdeb835550269fd97d8fe9
HyungtaeMoon <blessmht@gmail.com>
2019-05-22 13:07:06 +0900

    [MODEL][ADD] BugsChart 모델 migrate, User migrate 파일 커밋

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,3 +1,22 @@
+from tempfile import NamedTemporaryFile
+from urllib.request import urlopen
+
+from django.core.files import File
 from django.db import models
 
-# Create your models here.
+
+class BugsChart(models.Model):
+    ranking = models.PositiveSmallIntegerField()
+    image_file = models.ImageField(upload_to='bugs_album')
+    image_url = models.URLField()
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '벅스뮤직'
+        verbose_name_plural = f'{verbose_name} 목록'
+        ordering = ['-pk']
+
+    def __str__(self):
+        return self.title
