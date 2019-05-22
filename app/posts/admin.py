66875b48c9fc918bdf5adf24055b3c09ed19ab12
HyungtaeMoon <blessmht@gmail.com>
2019-05-22 13:07:54 +0900

    [ADD] BugsChart 모델 ModelAdmin 사용

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import BugsChart
+
+
+@admin.register(BugsChart)
+class BugsAdmin(admin.ModelAdmin):
+    list_display = ['ranking', 'photo_tag', 'title', 'artist', 'album']
+
+    list_display_links = ['title', 'artist']
+
+    def photo_tag(self, item):
+        if item.image_file:
+            return mark_safe('<img src={} style="width: 75px;"/>'.format(item.image_file.url))
+        return None
