5932d9a1ac3c2769a62a977c3ffeab992480dc00
HyungtaeMoon <blessmht@gmail.com>
2019-06-25 23:23:28 +0900

    [FIX] 주석 코드 삭제

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -6,19 +6,6 @@ from .base import *
 
 secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
 
-# Django 가 runserver 로 켜졌는지 확인
-# RUNSERVER = 'runserver' in sys.argv
-# DEBUG = False
-# ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
-
-# runserver 로 production 환경을 실행할 경우
-# if RUNSERVER:
-#     DEBUG = True
-    # ALLOWED_HOSTS = [
-    #     'localhost',
-    #     '127.0.0.1',
-    # ]
-
 # 아마존에서 제공해주는 URL에 접속을 허용하는 코드
 ALLOWED_HOSTS = [
     '.amazonaws.com',
@@ -39,49 +26,3 @@ WSGI_APPLICATION = 'config.wsgi.production.application'
 
 # DB
 DATABASES = secrets['DATABASES']
-
-# Log
-# LOG_DIR = '/var/log/django'
-# if not os.path.exists(LOG_DIR):
-#     LOG_DIR = os.path.join(ROOT_DIR, '.log')
-#     os.makedirs(LOG_DIR, exist_ok=True)
-
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'filters': {
-#         'require_debug_false': {
-#             '()': 'django.utils.log.RequireDebugFalse',
-#         },
-#         'require_debug_true': {
-#             '()': 'django.utils.log.RequireDebugTrue',
-#         },
-#     },
-#     'formatters': {
-#         'django.server': {
-#             'format': '[%(asctime)s] %(message)s',
-#         }
-#     },
-#     'handlers': {
-#         'console': {
-#             'level': 'INFO',
-#             'filters': ['require_debug_true'],
-#             'class': 'logging.StreamHandler',
-#         },
-#         'file_error': {
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'level': 'ERROR',
-#             'formatter': 'django.server',
-#             'backupCount': 10,
-#             'filename': os.path.join(LOG_DIR, 'error.log'),
-#             'maxBytes': 10485760,
-#         }
-#     },
-#     'loggers': {
-#         'django': {
-#             'handlers': ['console', 'file_error'],
-#             'level': 'INFO',
-#             'propagate': True,
-#         }
-#     }
-# }
